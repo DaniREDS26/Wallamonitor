97bce1bbcfbcd6e4c49ef98a765ee20faa622549
DaniREDS <daniel.martin.garcia.al@iespoligonosur.org>
2026-03-16 23:35:11 +0100

    Update telegram_manager.pyfix: replace asyncio event loop with asyncio.run() to prevent loop conflicts

--- managers/telegram_manager.py
+++ managers/telegram_manager.py
@@ -15,8 +15,6 @@ class TelegramManager:
         token, channel = self.get_config()
         self._channel = channel
         self._bot = telegram.Bot(token=token)
-        self._loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self._loop)
 
     def get_config(self):
         config_file = 'config.yaml'
@@ -27,19 +25,16 @@ class TelegramManager:
         return token, telegram_channel
 
     def send_telegram_article(self, article):
-        self._loop.run_until_complete(self.send_telegram_article_async(article))
+        asyncio.run(self.send_telegram_article_async(article))
 
     async def send_telegram_article_async(self, article):
-        # Insertamos los datos en la plantilla de texto plano
         message = ITEM_TEXT.format(
-            article.get_title(), 
+            article.get_title(),
             article.get_description(),
-            article.get_location(), 
-            article.get_price(), 
-            article.get_currency(), 
+            article.get_location(),
+            article.get_price(),
+            article.get_currency(),
             article.get_allows_shipping(),
             article.get_url()
         )
-        
-        # Enviamos el mensaje sin la etiqueta parse_mode
         await self._bot.send_message(chat_id=self._channel, text=message)
