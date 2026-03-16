e5e416e65c81596f2c1410fcd080164d9584e832
DaniREDS <daniel.martin.garcia.al@iespoligonosur.org>
2026-03-16 22:49:36 +0100

    fix: simplify telegram message format, remove markdown

--- managers/telegram_manager.py
+++ managers/telegram_manager.py
@@ -1,15 +1,14 @@
 import asyncio
 import yaml
 import telegram
-import re
 
-ITEM_TEXT = "- *Artículo*: {}\n" \
-            "- *Descripción*: {}\n" \
-            "- *Localidad*: {}\n" \
-            "- *Precio*: {} {}\n" \
-            "- *Acepta envíos*: {}\n" \
-            "[Ir al anuncio](https://es.wallapop.com/item/{})"
-
+# Plantilla simplificada sin negritas ni formatos Markdown
+ITEM_TEXT = "- Artículo: {}\n" \
+            "- Descripción: {}\n" \
+            "- Localidad: {}\n" \
+            "- Precio: {} {}\n" \
+            "- Acepta envíos: {}\n" \
+            "Enlace: https://es.wallapop.com/item/{}"
 
 class TelegramManager:
     def __init__(self):
@@ -27,19 +26,20 @@ class TelegramManager:
             telegram_channel = config['telegram_channel']
         return token, telegram_channel
 
-    
-    def escape_markdown(self, text):
-        special_chars = r'_[\]()~`>#\+\-=|{}.!]'
-        escaped_text = re.sub(f'([{re.escape(special_chars)}])', r'\\\1', text)
-        return escaped_text
-
     def send_telegram_article(self, article):
         self._loop.run_until_complete(self.send_telegram_article_async(article))
 
     async def send_telegram_article_async(self, article):
-        message = ITEM_TEXT.format(article.get_title(), self.escape_markdown(article.get_description()),
-                                   self.escape_markdown(article.get_location()), article.get_price(), 
-                                   article.get_currency(), article.get_allows_shipping(),
-                                   article.get_url())
-        escaped_message = self.escape_markdown(message)
-        await self._bot.send_message(self._channel, text=escaped_message, parse_mode="MarkdownV2")
+        # Insertamos los datos en la plantilla de texto plano
+        message = ITEM_TEXT.format(
+            article.get_title(), 
+            article.get_description(),
+            article.get_location(), 
+            article.get_price(), 
+            article.get_currency(), 
+            article.get_allows_shipping(),
+            article.get_url()
+        )
+        
+        # Enviamos el mensaje sin la etiqueta parse_mode
+        await self._bot.send_message(chat_id=self._channel, text=message)
